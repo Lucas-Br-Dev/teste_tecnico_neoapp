9e7cecd0175187974aa00801cd0a8c588081492d
Lucas <lucas.br.dev@gmail.com>
2025-08-06 15:15:00 -0300

    inicialização, estruturação e organização de pastas do projeto

---
+++ src/components/StyledComponents/Header.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+ export const Header = styled.header`
+    height: 40px;
+    background-color: red;
+    padding: 10px;
+    display: flex;
+ `
---
+++ src/components/StyledComponents/Navegation.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const NavMenu = styled.div`
+    display: flex;
+`
